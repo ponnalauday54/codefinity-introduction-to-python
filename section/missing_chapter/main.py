9c2d2fe175d333a1bdca6c7c8e24a6e57f92614c
ponnalauday54 <>
2026-04-07 21:16:03 +0000

    Missing chapter

--- section/missing_chapter/main.py
+++ section/missing_chapter/main.py
@@ -1,9 +1,9 @@
 print("Shopping Receipt")
 print("-----------------------")
-print("Item 1: $", 162*1.2)  # Price per weight
-print("Item 2: $", 25-3)  # Discounted price
+print("Item 1: $", 162 * 1.2)  # Price per weight
+print("Item 2: $", 25 - 3)  # Discounted price
 print("-----------------------")
-print("Subtotal: $","Item 1 + Item 2")
-print("Tax (8%): $", "subtotal * 0.08")
+print("Subtotal: $", (162 * 1.2) + (25 - 3))
+print("Tax (8%): $", ((162 * 1.2) + (25 - 3)) * 0.08)
 print("-----------------------")
 print("Thank you for shopping!")
